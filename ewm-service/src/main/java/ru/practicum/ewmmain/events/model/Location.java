83ff7e56cfc5d8967d256fb5f10bf5e1d0a54b6d
Evgeniy <jeka408@yandex.ru>
2022-12-04 23:14:41 +0300

    -refactor. delete Unused import

--- ewm-service/src/main/java/ru/practicum/ewmmain/events/model/Location.java
+++ ewm-service/src/main/java/ru/practicum/ewmmain/events/model/Location.java
@@ -1,10 +1,6 @@
 package ru.practicum.ewmmain.events.model;
 
 import lombok.Data;
-import ru.practicum.ewmmain.utill.Create;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class Location {
